356d67b819a21e66a01ccdbdcc95f5dce0827c14
Mike McAvoy <218127259+mikemcavoydev@users.noreply.github.com>
2025-10-03 18:09:05 +0100

    feat: view snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+	"text/template"
+
+	"github.com/mikemcavoydev/snippetbox/internal/models"
 )
 
 func (a *application) home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Server", "Go")
+	snippets, err := a.snippets.Latest()
+	if err != nil {
+		a.serverError(w, r, err)
+		return
+	}
 
 	tmpls := []string{
 		"./ui/html/pages/base.tmpl.html",
@@ -22,7 +29,11 @@ func (a *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
+	data := templateData{
+		Snippets: snippets,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		a.serverError(w, r, err)
 	}
@@ -36,7 +47,37 @@ func (a *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Displaying specific snippet with ID: %d", id)
+	snippet, err := a.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+
+		} else {
+			a.serverError(w, r, err)
+		}
+		return
+	}
+
+	tmpls := []string{
+		"./ui/html/pages/base.tmpl.html",
+		"./ui/html/partials/navigation.tmpl.html",
+		"./ui/html/pages/view.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(tmpls...)
+	if err != nil {
+		a.serverError(w, r, err)
+		return
+	}
+
+	data := templateData{
+		Snippet: snippet,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		a.serverError(w, r, err)
+	}
 }
 
 func (a *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
